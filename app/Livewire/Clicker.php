e953ec42ac068e82748767cacc81cb3345a222de
António Camarão <camaraoacsn@gmail.com>
2023-09-26 10:26:44 +0100

    Episode 5 - Flash Messages

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -2,38 +2,44 @@
 
 namespace App\Livewire;
 
+
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Clicker extends Component
 {
     // Este era o método novo para se fazer as validações, o antigo é o que está a ser usado dentro do createNewUser. O Novo método pode ser usado a partir do PHP ver.8.1
     // #[Rule('required|min|max:50')]
-    // public $name;
+    // public $name = '';
 
     // #[Rule('required|email|unique:users')]
-    // public $email;
+    // public $email = '';
 
     // #[Rule('required|min:5')]
-    // public $password;
+    // public $password = '';
 
-    public $name;
-    public $email;
-    public $password;
+    public $name = '';
+    public $email = '';
+    public $password = '';
 
     public function createNewUser() {
         // $this->validate();
-        $this->validate([
+        $validated = $this->validate([
             'name' => 'required|min:2|',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:5'
         ]);
 
         User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => $this->password
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
         ]);
+
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'User Created Successfully!');
     }
 
     public function render()
