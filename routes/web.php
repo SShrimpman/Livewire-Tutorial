2310ec68b14c8e615a21c3da847213828056a57f
António Camarão <camaraoacsn@gmail.com>
2023-09-29 14:38:02 +0100

    Build Data Table with Livewire 3

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,16 @@ use App\Livewire\UsersPage;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function () {
+//     return view('crud');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('data-table');
 });
 
 // Route::get('/', HomePage::class);
